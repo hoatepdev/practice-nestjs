a3889fec71e526a845ee2df6292723feba493073
hoatep <hoa.nt2@isofh.com>
2023-07-10 18:01:54 +0700

    Add controller & module

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  Body,
+  Query,
+  UsePipes,
+  ValidationPipe,
+  ParseBoolPipe,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers(@Query('sortDesc', ParseBoolPipe) sortBy: boolean) {
+    return [
+      {
+        username: 'hoatep',
+        email: 'hoa@gmail.com',
+      },
+    ];
+  }
+
+  @Post('create')
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData);
+  }
+
+  @Get(':id/:postId')
+  getUserById(@Param('id') id: string) {
+    console.log(id);
+    return { id };
+  }
+}
